3798d161ff6b0f7a92d350a03e052d14d31c4b13
sinned8 <dennisdust2@gmail.com>
2023-10-23 11:14:07 -0400

    Added grid for the game grid element. Added styling and necessary JS to all buttons. Page skeleton complete.

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+
 // Selectors for chosing opponent screen & player or AI selection
-// Event listeners to hide and show respect dives on bttn press
+// Event listeners to hide and show respect divs and change header on bttn press
 const introContainer = document.getElementById('intro')
 const chooseOpponentBttn = document.getElementById('opponet-selector')
-const modalSelection = document.getElementById('modal-container');
-chooseOpponentBttn.addEventListener('click', () => modalSelection.style.display = 'block')
+const modalSelection = document.getElementById('modal-container')
+chooseOpponentBttn.addEventListener('click', () => modalSelection.style.display = 'flex')
 chooseOpponentBttn.addEventListener('click', () => introContainer.style.display = 'none')
 
 const gameContainer = document.getElementById('game-container')
 const playPersonBttn = document.getElementById('play-person')
 const playAiBttn = document.getElementById('player-ai')
+const gameHeader = document.getElementById('game-header')
 
-playPersonBttn.addEventListener('click', () => gameContainer.style.display = 'block')
+playPersonBttn.addEventListener('click', () => gameContainer.style.display = 'flex')
 playPersonBttn.addEventListener('click', () => modalSelection.style.display = 'none')
-playAiBttn.addEventListener('click', () => gameContainer.style.display = 'block')
+playPersonBttn.addEventListener('click', () => gameHeader.innerHTML = 'Player vs Player')
+playAiBttn.addEventListener('click', () => gameContainer.style.display = 'flex')
 playAiBttn.addEventListener('click', () => modalSelection.style.display = 'none')
+playAiBttn.addEventListener('click', () => gameHeader.innerHTML = 'Player vs AI')
+
+const mainMenu = document.getElementById('main-menu')
+mainMenu.addEventListener('click', () => gameContainer.style.display = 'none')
+mainMenu.addEventListener('click', () => introContainer.style.display = 'flex')
+
+
+
 
 
 
